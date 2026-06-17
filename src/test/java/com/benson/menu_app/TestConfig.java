938235b87a637a71326784b47460906d625e86e4
Andrew Benson <andrewbenson123@gmail.com>
2026-06-17 14:20:04 -0400

    Fixed tests

--- src/test/java/com/benson/menu_app/TestConfig.java
+++ src/test/java/com/benson/menu_app/TestConfig.java
@@ -1,5 +1,6 @@
 package com.benson.menu_app;
 
+import com.benson.menu_app.security.JwtUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,11 @@ public class TestConfig {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    @Bean
+    public JwtUtil jwtUtil() {
+        return new JwtUtil();
     }
 }
 
